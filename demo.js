9f5d92f4010cf6390a18d8aa016b2d5212a5978d
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2019-05-02 10:14:04 +0530

    Landing Page completed

---
+++ demo.js
@@ -0,0 +1,18 @@
+const items = document.querySelectorAll('.flex-gallery__item');
+
+items.forEach( item => item.addEventListener('mouseover', showItem));
+items.forEach( item => item.addEventListener('transitionend', activeItem));
+
+function showItem ( e ) {
+  if(this.classList.contains('active')){
+    return false;
+  }
+  items.forEach(item => item.classList.remove('active'));
+  this.classList.toggle('active');
+};
+
+function activeItem ( e ) {
+  if ( e.propertyName.includes('flex') ) {
+    this.classList.toggle('visible');
+  }
+};
