4d5c78d44773528f24e251b1c8ddffa16090d092
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2019-06-28 16:56:46 +0530

    Final changes

--- White/public/script/scriptProj.js
+++
@@ -1,9 +0,0 @@
-// Initialize
-const allCardsHeaders = document.getElementsByClassName("card-header");
-for(header of allCardsHeaders) {
-    header.addEventListener("click", clickCard);
-}
-
-function clickCard(e) {
-    this.parentElement.classList.toggle('cardActive');
-}
